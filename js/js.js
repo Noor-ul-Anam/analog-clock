9be44211118f1b617bbed9ab8e02c0651819b179
Noor-ul-Anam <anam.xyed97@gmail.com>
2019-09-10 01:09:22 +0500

    clock running with issues

--- js/js.js
+++ js/js.js
@@ -1,3 +1,8 @@
+// --- variables ---
+let degree= 0;
+
+
+// --- creating graduations in teh clock..
 let graduation = () => {
     let element = document.getElementById('graduations');
     for(i= 1; i<13; i++){
@@ -6,4 +11,21 @@ let graduation = () => {
 }
 graduation();
 
-// console.log(moment().fromNow());
+// --- setting up time in teh clock using date methods..
+let date= new Date();
+console.log(date.getHours()-12);
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+setTimeout(() => {
+    let sec = document.getElementById('needleSecond');
+    degree= degree+6;
+    console.log('abs');
+    sec.style.transform = "translate(50%,75%) rotate(${degree}deg)";
+    console.log('abs');
+}, 1000);
+// setInterval(() => {
+//     let sec = document.getElementById('needleSecond');
+//     degree= degree+6;
+//     sec.style.transform = `rotate(${degree}deg) translate(50%,75%)`;
+// }, 1000);
