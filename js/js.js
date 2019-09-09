59a02e873e962b93792946f9d8a3a303156e18ed
Noor-ul-Anam <anam.xyed97@gmail.com>
2019-09-09 19:12:15 +0500

    design finished

--- js/js.js
+++ js/js.js
@@ -1,7 +1,9 @@
 let graduation = () => {
     let element = document.getElementById('graduations');
     for(i= 1; i<13; i++){
-        element.innerHTML += `<h1 class="graduation" id=${i} >${i}</h1>`;
+        element.innerHTML += `<h1 class="graduation">${i}</h1>`;
     }
 }
 graduation();
+
+// console.log(moment().fromNow());
